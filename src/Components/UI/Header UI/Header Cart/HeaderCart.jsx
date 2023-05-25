2abf6c7995f7a38d867761b39a921bd545ef5efb
its-rajesh-smp <its.rajeshsmp@gmail.com>
2023-05-25 08:37:17 +0530

    User Can See Cart Quantity Amount In Header

--- src/Components/UI/Header UI/Header Cart/HeaderCart.jsx
+++ src/Components/UI/Header UI/Header Cart/HeaderCart.jsx
@@ -1,13 +1,18 @@
 import React from "react";
 import "./HeaderCart.css";
+import { useSelector } from "react-redux";
 
 function HeaderCart(props) {
+  const cartTotalQuantity = useSelector(
+    (state) => state.totalAmountSlice.amountDetails.totalQuantity
+  );
+
   return (
     <div className=" HeaderCart-div ">
       <div>
         <i className="bx bxs-cart-alt"></i>
         <div>
-          <span>2</span>
+          <span>{cartTotalQuantity}</span>
         </div>
       </div>
       <p>Cart</p>
